712527dcdb0c2b2be5bd0d3d2a83ae2fe00a3445
Dmitry Kudriavtsev <dkudriavtsev@gmail.com>
2017-04-24 08:31:55 -0700

    Changes

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,3 +1,13 @@
 class Tool < ApplicationRecord
-    monetize :price, as: :price_dollars
+	monetize :price, as: :price_dollars
+	def grade
+		if self.grade_low == self.grade_high then
+			self.grade_low == 0 ? 'K' : self.grade_low
+		else
+			"#{self.grade_low == 0 ? 'K' : self.grade_low}-#{self.grade_high}"
+		end
+	end
+	def full_price
+		'$' + self.price_dollars.to_s
+	end
 end
